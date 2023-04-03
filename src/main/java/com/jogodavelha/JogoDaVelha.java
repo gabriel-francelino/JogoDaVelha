d83e574cfc84efac6beb16b5ac4021ffefa34103
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2023-04-03 15:02:50 -0300

    classe JogoDaVelha crianda

--- src/main/java/com/jogodavelha/JogoDaVelha.java
+++ src/main/java/com/jogodavelha/JogoDaVelha.java
@@ -1,5 +1,53 @@
 package com.jogodavelha;
 
+import java.util.Scanner;
+
 public class JogoDaVelha {
+    private Campo[][] velha;
+    private String simboloAtual;
+    private Boolean jogo;
+    private String vitoria;
+    private Scanner scan;
+
+    public JogoDaVelha() {
+        this.velha = new Campo[3][3];
+        this.simboloAtual = "X";
+        this.jogo = true;
+        this.vitoria = "";
+        this.scan = new Scanner(System.in);
+    }
+
+    public Campo[][] getVelha() {
+        return velha;
+    }
+
+    public void setVelha(Campo[][] velha) {
+        this.velha = velha;
+    }
+
+    public String getSimboloAtual() {
+        return simboloAtual;
+    }
+
+    public void setSimboloAtual(String simboloAtual) {
+        this.simboloAtual = simboloAtual;
+    }
+
+    public Boolean getJogo() {
+        return jogo;
+    }
+
+    public void setJogo(Boolean jogo) {
+        this.jogo = jogo;
+    }
+
+    public String getVitoria() {
+        return vitoria;
+    }
+
+    public void setVitoria(String vitoria) {
+        this.vitoria = vitoria;
+    }
+
     
 }
